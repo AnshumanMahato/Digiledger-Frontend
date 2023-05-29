74fae4f662ced04a6c1c7930645b6c2cd6de7b5c
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2023-05-30 05:17:04 +0530

    balance card done

--- src/components/BalanceCard.jsx
+++ src/components/BalanceCard.jsx
@@ -1,14 +1,16 @@
 function BalanceCard() {
   return (
     <>
-      <h2 className=" text-3xl font-bold mb-5 w-full">Current Month</h2>
+      <h2 className=" text-3xl relative z-10 font-bold mb-8 w-full">
+        Current Month
+      </h2>
       <div
         className="container relative h-min 
       before:content-[''] before:absolute before:bg-gradient-to-r before:from-primary before:to-secondary 
       before:top-0 before:left-0 before:h-[90%] before:w-[85%] before:rounded-2xl
-      before:transform before:origin-top-left before:-rotate-[10deg]"
+      before:transform before:origin-top-left before:-rotate-[8deg]"
       >
-        <div className="w-full p-8 bg-white/5 relative rounded-2xl backdrop-blur-md shadow-inner-lg shadow-white">
+        <div className="w-full p-8 bg-white/5 relative rounded-2xl backdrop-blur-md shadow-md">
           <div className="flex flex-col mb-5">
             <span className="text-xl mb-1">Balance</span>
             <span className=" text-4xl font-bold">$2000</span>
