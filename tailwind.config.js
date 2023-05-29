5300d06d54fa8c6df818fd7d91010e3b61794b07
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2023-05-29 10:13:09 +0530

    tailwind setup:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      primary: "#302d43",
+      secondary: "#6953f7",
+      accent: "#cd4ff7",
+    },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
