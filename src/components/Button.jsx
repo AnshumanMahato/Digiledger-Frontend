f1f9d4830ad6369898524d19e82ce7507d7021d1
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2023-05-31 19:02:41 +0530

    dashboard page initial done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,9 @@
 function Button({ children }) {
-  return <button>{children}</button>;
+  return (
+    <button className=" px-5 font-bold text-2xl py-3 rounded-xl glass bg-secondary">
+      {children}
+    </button>
+  );
 }
 
 export default Button;
