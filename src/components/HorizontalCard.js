c5e58c7fcb895da87b5e34e13279934a88f8fbbb
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2023-05-31 05:50:32 +0530

    card init

--- src/components/HorizontalCard.js
+++ src/components/HorizontalCard.js
@@ -2,12 +2,15 @@ import React from "react";
 
 const HorizontalCard = ({ name, date, time, amount }) => {
   return (
-    <div className="rounded-lg p-4 flex glass">
+    <div className="rounded-lg p-4 flex">
       <div className="flex-grow">
         <p className="text-lg font-bold">{name}</p>
         <p className="text-sm text-gray-500">{`${date} ${time}`}</p>
       </div>
-      <div className="flex-none">
+      <div className="flex justify-center items-center pl-10">
+        <p className="text-lg font-bold">{amount}</p>
+      </div>
+      <div className="flex justify-center items-center pl-10">
         <p className="text-lg font-bold">{amount}</p>
       </div>
     </div>
