41b2c53cc6f88cf44d736d1ebfb3f88c9385a55a
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2023-06-02 18:17:59 +0530

    buttons added

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -7,7 +7,6 @@ function Root() {
   return (
     <div className="min-h-screen w-screen text-white bg-accent flex flex-col justify-between items-center pt-10 px-[10%] font-poppins">
       <Header />
-      <Nav />
       <Outlet />
       <Footer />
     </div>
