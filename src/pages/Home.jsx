6132af0bd0c0625f18eea8d29cd07422e038d2df
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2023-07-09 11:53:21 +0530

    Feature Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,11 +8,17 @@ import { AiOutlineTransaction, AiTwotonePieChart } from "react-icons/ai";
 import { GiReceiveMoney } from "react-icons/gi";
 import FeatureCard from "../components/FeatureCard";
 
+/*
+I have put all sections of this in a single component as none of these seem reusable
+and I did not want to make extra components unnecessarily
+*/
+
 const LandingPage = () => {
   const { currentUser } = useUserContext();
 
   return (
     <>
+      {/* Hero Section */}
       <Section className="min-h-[75vh] flex flex-col xl:flex-row items-center justify-evenly">
         <div className="w-full xl:w-2/3 border-b-4">
           <img src={hero} alt="woman-budgeting-hero" />
@@ -34,6 +40,7 @@ const LandingPage = () => {
         </div>
       </Section>
 
+      {/* About Section */}
       <Section className="grid gap-6 grid-cols-1 grid-rows-[repeat(3,min-content)] lg:grid-rows-[repeat(2,min-content)] lg:grid-cols-2 justify-evenly bg-accent-dark/50 py-[7%]">
         <SectionHeader className="text-center col-span-full">
           About Digiledger
@@ -56,6 +63,7 @@ const LandingPage = () => {
         </div>
       </Section>
 
+      {/* Feature Section */}
       <Section className="py-[7%] grid grid-cols-1 grid-rows-[min-content_repeat(3,1fr)] sm:grid-cols-3 sm:grid-rows-[min-content_1fr] gap-8">
         <SectionHeader className="sm:col-span-full text-center">
           Features
